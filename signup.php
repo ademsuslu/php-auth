407ac31f3f17580404ace8ccbf83e82fdc2e232c
ademsuslu <ademsuslu9080@gmail.com>
2024-09-30 18:21:05 +0300

    refactor:signup with condirions complated

--- signup.php
+++ signup.php
@@ -1,4 +1,50 @@
 <?php
 
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        include ('connect.php');
+        if(isset($_POST['signup'])){
+            $username=$_POST['username'];
+            $password=md5($_POST['password']);
+            $confirmpassword=md5($_POST['confirmpassword']);
+        }{
+
+
+            // $sql="insert into  `data` (username,password) values('$username','$password')";
+            // $result = mysqli_query($con,$sql);
+            // if($result){
+            //     echo "You have successfully registered";
+            // }else {
+            //     echo "Error: ". $sql. "<br>". mysqli_error($con);
+            // }
+
+            //! username is already registered ? check your username
+            $sql = "SELECT * FROM `data` WHERE `username` = '$username'";
+            $result = mysqli_query($con,$sql);
+            if($result){
+                $num = mysqli_num_rows($result);
+                if($num > 0){
+                    echo "Username already exists";
+                } else {
+                      if($password !== $confirmpassword){
+                        echo "Password is incorrect";
+                      }else{
+                        $sql="insert into  `data` (username,password) values('$username','$password')";
+                        $result = mysqli_query($con,$sql);
+                       if($result){
+                           echo "You have successfully registered";
+                       }else {
+                            die(mysqli_error($con));
+                       }
+                      }
+                }
+
+            }else {
+                 die(mysqli_error($con));
+            }
+
+
+        }
+    }
+
 
 ?>
